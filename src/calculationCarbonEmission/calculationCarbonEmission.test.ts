c7e7e81cf0a3ca149b8816c8093e44311674e8c5
moreasar <sarah.moreau@grenoble-inp.org>
2025-01-29 18:29:10 +0100

    add calculateIngredientEmission and calculateRecipeEmission functions

---
+++ src/calculationCarbonEmission/calculationCarbonEmission.test.ts
@@ -0,0 +1,83 @@
+import { calculateIngredientEmission, calculateRecipeEmission } from "./calculationCarbonEmission";
+
+const hamCheesePizza = {
+    ingredients: [
+        { name: "ham", quantity: 0.1, unit: "kg" },
+        { name: "cheese", quantity: 0.15, unit: "kg" },
+        { name: "tomato", quantity: 0.4, unit: "kg" },
+        { name: "flour", quantity: 0.7, unit: "kg" },
+        { name: "oliveOil", quantity: 0.3, unit: "kg" },
+    ],
+};
+
+const hamCheesePizzaWithFloor = {
+    ingredients: [
+        { name: "ham", quantity: 0.1, unit: "kg" },
+        { name: "cheese", quantity: 0.15, unit: "kg" },
+        { name: "tomato", quantity: 0.4, unit: "kg" },
+        { name: "floor", quantity: 0.7, unit: "kg" },
+        { name: "oliveOil", quantity: 0.3, unit: "kg" },
+    ],
+};
+
+const hamCheesePizzaWithNegativeQuantitiy = {
+    ingredients: [
+        { name: "ham", quantity: 0.1, unit: "kg" },
+        { name: "cheese", quantity: -0.15, unit: "kg" },
+        { name: "tomato", quantity: 0.4, unit: "kg" },
+        { name: "flour", quantity: 0.7, unit: "kg" },
+        { name: "oliveOil", quantity: 0.3, unit: "kg" },
+    ],
+};
+
+const hamCheesePizzaWithNoCheese = {
+    ingredients: [
+        { name: "ham", quantity: 0.1, unit: "kg" },
+        { name: "cheese", quantity: 0, unit: "kg" },
+        { name: "tomato", quantity: 0.4, unit: "kg" },
+        { name: "flour", quantity: 0.7, unit: "kg" },
+        { name: "oliveOil", quantity: 0.3, unit: "kg" },
+    ],
+};
+
+const cheesePizza = {
+    ingredients: [
+        { name: "cheese", quantity: 250, unit: "g" },
+        { name: "blueCheese", quantity: 100, unit: "g" },
+        { name: "flour", quantity: 0.7, unit: "kg" },
+        { name: "oliveOil", quantity: 0.3, unit: "kg" },
+    ],
+};
+
+describe("CalculateIngredientEmission", () => {
+    it("should return 0.011 if the quantity is 0.1kg and the emission of CO2 per Kg is 0.11", () => {
+        expect(calculateIngredientEmission({ name: "ham", quantity: 0.1, unit: "kg" })).toBe(0.011);
+    });
+
+    it("should return 0.011 if the quantity is 100g and the emission of CO2 per Kg is 0.11", () => {
+        expect(calculateIngredientEmission({ name: "ham", quantity: 100, unit: "g" })).toBe(0.011);
+    });
+
+});
+
+describe("CalculateRecipeEmission", () => {
+    it("should return 0.224 for the C02 emission of hamCheesePizza", () => {
+        expect(calculateRecipeEmission(hamCheesePizza)).toBe(0.224);
+    });
+
+    it("should return null because of an EmissionFactorError", () => {
+        expect(calculateRecipeEmission(hamCheesePizzaWithFloor)).toBe(null);
+    });
+
+    it("should return null because of an QuantityError", () => {
+        expect(calculateRecipeEmission(hamCheesePizzaWithNegativeQuantitiy)).toBe(null);
+    });
+
+    it("should return 0.224 for the C02 emission of hamCheesePizzaWithNoCheese because cheese quantity = 0", () => {
+        expect(calculateRecipeEmission(hamCheesePizzaWithNoCheese)).toBe(0.206);
+    });
+
+    it("should return 0.207 for the C02 emission of cheesePizza", () => {
+        expect(calculateRecipeEmission(cheesePizza)).toBe(0.207);
+    });
+});
---
+++ src/calculationCarbonEmission/calculationCarbonEmission.ts
@@ -0,0 +1,56 @@
+import { round } from "lodash";
+import { getTestEmissionFactor } from "../seed-dev-data";
+import { convertQuantityToKg } from "./conversionToKg";
+import { EmissionFactorError, QuantityError, UnknownUnitError } from "./errors";
+import { Recipe, RecipeIngredient } from "./recipe.entity";
+
+
+const hamCheesePizza = {
+    ingredients: [
+        { name: "ham", quantity: 0.1, unit: "kg" },
+        { name: "cheese", quantity: 0.15, unit: "kg" },
+        { name: "tomato", quantity: 0.4, unit: "kg" },
+        { name: "flour", quantity: 0.7, unit: "kg" }, //bug fix floor to flour
+        { name: "oliveOil", quantity: 0.3, unit: "kg" },
+    ],
+};
+
+export const calculateIngredientEmission = (ingredient: RecipeIngredient): number => {
+    const emissionFactor = getTestEmissionFactor(ingredient.name);
+
+    let co2EmissionPerKg: number = emissionFactor.emissionCO2eInKgPerUnit
+    let quantityIngredient: number = ingredient.quantity;
+
+    if (emissionFactor.unit != "kg") {
+        co2EmissionPerKg = convertQuantityToKg(emissionFactor.unit, co2EmissionPerKg);
+    }
+
+    if (ingredient.unit != "kg") {
+        quantityIngredient = convertQuantityToKg(ingredient.unit, quantityIngredient);
+    }
+
+    return round(quantityIngredient * co2EmissionPerKg, 3);
+}
+
+export const calculateRecipeEmission = (recipe: Recipe): number | null => {
+    let recipeEmission: number = 0;
+
+    for (let i = 0; i < recipe.ingredients.length; i = i + 1) {
+        try {
+            recipeEmission += calculateIngredientEmission(recipe.ingredients[i])
+        }
+        catch (error) {
+            if (error instanceof UnknownUnitError) {
+                console.log("Unit can't be convert to kg.");
+            }
+            if (error instanceof QuantityError) {
+                console.log("Quantity value is negative.");
+            }
+            if (error instanceof EmissionFactorError) {
+                console.log("Emission factor doesn't exist.");
+            }
+            return null;
+        }
+    }
+    return round(recipeEmission, 3);
+}
